771112aca9d7de87aa1a5e22d77d601c3d99691d
Denny Bucklin <dennis.bucklin@gmail.com>
2024-04-15 12:29:51 -0500

    SUU route

--- function_app.py
+++ function_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import azure.functions as func
 import logging
-from acl.calendar_fetch import build_dates, get_TXST_holidays
+from acl.calendar_fetch import build_dates, get_SUU_holidays, get_TXST_holidays
 import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -43,6 +43,36 @@ def TXST_calendar(req: func.HttpRequest) -> func.HttpResponse:
         holidays
     )
     logging.info("Returning calendar")
+    return func.HttpResponse(
+        json.dumps(class_dates),
+        mimetype="application/json",
+        status_code=200
+    )
+
+
+@app.route(route="SUU_calendar")
+def SUU_calendar(req: func.HttpRequest) -> func.HttpResponse:
+    '''
+    On GET request returns a dictionary with three keys:
+        "dates": list of dates the class meets,
+        "topics": list of blanks for filling in topic for the day,
+        "assignments": list that is blank except on holidays,
+    '''
+    logging.info('Retrieving SUU calendar')
+
+    start = datetime.strptime(req.route_params.get('start'), "%m%d%y")
+    end = datetime.strptime(req.route_params.get('end'), "%m%d%y")
+    weekdays = req.route_params.get('weekdays')
+    logging.info(f"Getting SUU holidays from {start} to {end} on {weekdays}")
+    holidays = get_SUU_holidays(start, end)
+    logging.info(f"Dates returned: {holidays}")
+    class_dates = build_dates(
+        start,
+        end,
+        weekdays,
+        holidays
+    )
+    logging.info('Returning calendar')
     return func.HttpResponse(
         json.dumps(class_dates),
         mimetype="application/json",
